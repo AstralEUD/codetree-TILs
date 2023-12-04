fdc512f563ad22321a3e5f0ac7aa77be21370955
Testify <50492313+AstralEUD@users.noreply.github.com>
2023-12-04 20:47:11 +0900

    Title: 좌표평면 위의 편안한 상태, Time: 1088ms, Memory: 4MB, Status: Time Limit Exceed - CodeTree

--- 231204/좌표평면 위의 편안한 상태/comfortable-state-on-the-2d-coordinate.c
+++ 231204/좌표평면 위의 편안한 상태/comfortable-state-on-the-2d-coordinate.c
@@ -2,32 +2,35 @@
 
 int main() {
     int arr[1005][1005] = {0};
+    int subarr[100000][2] = {0};
     int n, i, p, q, x, y, v, z, cnt=0;
     scanf("%d",&n);
     for (p=1; p<=n; p++) {
         scanf("%d %d",&x,&y);
         x++; y++;
         arr[x][y] = 1;
+        subarr[p][0] = x;
+        subarr[p][1] = y;
         cnt=0;
-        for (v=1; v<=1001; v++) {
-            for (z=1; z<=1001; z++) {
-                if (arr[v][z] == 1) {
-                    i=0;
-                    if (arr[v][z+1] == 1) {
-                        i++;
-                    } 
-                    if (arr[v][z-1] == 1) {
-                        i++;
-                    }
-                    if (arr[v+1][z] == 1) {
-                        i++;
-                    }
-                    if (arr[v-1][z] == 1) {
-                        i++;
-                    }
-                    if (i==3) {
-                        cnt++;
-                    }
+        for (q=1; q<=p; q++) {
+            v=subarr[q][0];
+            z=subarr[q][1];
+            if (arr[v][z] == 1) {
+                i=0;
+                if (arr[v][z+1] == 1) {
+                    i++;
+                } 
+                if (arr[v][z-1] == 1) {
+                    i++;
+                }
+                if (arr[v+1][z] == 1) {
+                    i++;
+                }
+                if (arr[v-1][z] == 1) {
+                    i++;
+                }
+                if (i==3) {
+                    cnt++;
                 }
             }
         }
