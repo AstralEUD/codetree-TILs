bdc72eb0e6ced9b54c98dd1ae1b088b589d04a62
Testify <50492313+AstralEUD@users.noreply.github.com>
2023-12-04 21:24:24 +0900

    Title: 규칙에 따른 수열, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231204/규칙에 따른 수열/sequence-according-to-the-rules.c
+++ 231204/규칙에 따른 수열/sequence-according-to-the-rules.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
+int arr[100] = {0};
+
 int suwol(int n) {
     int i, t , cnt, v, q=0;
     i=0; v=n-1;cnt=0;
     if((n==0) || (n==1)) {
+        arr[n] = 1;
         return 1;
     }
     while ((i<=n-1) && (v>=0)) {
-        q=suwol(i) * suwol(v);
+        q=arr[i] * arr[v];
         cnt+=q;
         i++;
         v--;
     }
+    arr[n] = cnt;
     return cnt;
 }
 
 int main() {
     int n, i, t;
     scanf("%d",&n);
-    t = suwol(n);
+    for (i=0; i<=n; i++) {
+        t = suwol(i);
+    }
     printf("%d",t);
     return 0;
 }
