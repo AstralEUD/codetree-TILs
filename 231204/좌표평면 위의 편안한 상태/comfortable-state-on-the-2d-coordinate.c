f9c9a4abf0ef29cdf61bb843cd6a34eda60139e6
Testify <50492313+AstralEUD@users.noreply.github.com>
2023-12-04 20:28:01 +0900

    Title: 좌표평면 위의 편안한 상태, Time: 8ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 231204/좌표평면 위의 편안한 상태/comfortable-state-on-the-2d-coordinate.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[1005][1005] = {0};
+    int n, i, p, q, x, y, v, z, cnt=0;
+    scanf("%d",&n);
+    for (p=1; p<=n; p++) {
+        scanf("%d %d",&x,&y);
+        arr[x][y] = 1;
+        cnt=0;
+        for (v=1; v<=n; v++) {
+            for (z=1; z<=n; z++) {
+                i=0;
+                if (arr[v][z+1]) {
+                    i++;
+                } 
+                if (arr[v][z-1]) {
+                    i++;
+                }
+                if (arr[v+1][z]) {
+                    i++;
+                }
+                if (arr[v-1][z]) {
+                    i++;
+                }
+                if (i==3) {
+                    cnt++;
+                }
+            }
+        }
+        printf("%d\n",cnt);
+    }
+    return 0;
+}
