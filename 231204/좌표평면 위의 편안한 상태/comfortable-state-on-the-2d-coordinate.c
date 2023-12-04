ab6621edb05c75e4b7035b0b1c3b4bcba19e7d65
Testify <50492313+AstralEUD@users.noreply.github.com>
2023-12-04 21:04:03 +0900

    Title: 좌표평면 위의 편안한 상태, Time: 51ms, Memory: 3MB, Status: Passed - CodeTree

--- 231204/좌표평면 위의 편안한 상태/comfortable-state-on-the-2d-coordinate.c
+++ 231204/좌표평면 위의 편안한 상태/comfortable-state-on-the-2d-coordinate.c
@@ -1,39 +1,73 @@
 #include <stdio.h>
+int arr[1005][1005] = {0};
+int cnt=0;
+
+void check_2 (int v, int z) {
+    int i;
+    if (arr[v][z] == 1) {
+        i=0;
+        if (arr[v][z+1] == 1) {
+            i++;
+        } 
+        if (arr[v][z-1] == 1) {
+            i++;
+        }
+        if (arr[v+1][z] == 1) {
+            i++;
+        }
+        if (arr[v-1][z] == 1) {
+            i++;
+        }
+        if (i==3) {
+            cnt--;
+        }
+    }
+    return;
+}
+
+
+void check (int v, int z, int q){
+    int i;
+    if (arr[v][z] == 1) {
+        i=0;
+        if (arr[v][z+1] == 1) {
+            i++;
+        } 
+        if (arr[v][z-1] == 1) {
+            i++;
+        }
+        if (arr[v+1][z] == 1) {
+            i++;
+        }
+        if (arr[v-1][z] == 1) {
+            i++;
+        }
+        if (i==3) {
+            cnt++;
+        }
+    }
+    return;
+}
 
 int main() {
-    int arr[1005][1005] = {0};
-    int subarr[100000][2] = {0};
-    int n, i, p, q, x, y, v, z, cnt=0;
+
+    int n, i, p, q, x, y, v, z;
     scanf("%d",&n);
     for (p=1; p<=n; p++) {
         scanf("%d %d",&x,&y);
         x++; y++;
+        check_2(x+1,y);
+        check_2(x-1,y);
+        check_2(x,y+1);
+        check_2(x,y-1);
+
         arr[x][y] = 1;
-        subarr[p][0] = x;
-        subarr[p][1] = y;
-        cnt=0;
-        for (q=1; q<=p; q++) {
-            v=subarr[q][0];
-            z=subarr[q][1];
-            if (arr[v][z] == 1) {
-                i=0;
-                if (arr[v][z+1] == 1) {
-                    i++;
-                } 
-                if (arr[v][z-1] == 1) {
-                    i++;
-                }
-                if (arr[v+1][z] == 1) {
-                    i++;
-                }
-                if (arr[v-1][z] == 1) {
-                    i++;
-                }
-                if (i==3) {
-                    cnt++;
-                }
-            }
-        }
+        check(x,y,p);
+        check(x+1,y,p);
+        check(x-1,y,p);
+        check(x,y+1,p);
+        check(x,y-1,p);
+
         printf("%d\n",cnt);
     }
     return 0;
